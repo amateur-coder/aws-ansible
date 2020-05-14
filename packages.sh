fa734ad1c76d4d864f35779d9653d35d87904d5e
singh-ashok25 <ashok_iitr@yahoo.com>
2020-05-14 22:42:02 +0100

    upd pkg

--- packages.sh
+++ packages.sh
@@ -4,6 +4,7 @@ sudo yum install python3 -y
 sudo amazon-linux-extras install ansible2 -y
 echo "==================================================="
 echo "RUN below command manually"
+echo "==================================================="
 echo python3 -m venv ansible
 echo source ~/aws-ansible/ansible/bin/activate
 echo pip install pip --upgrade
