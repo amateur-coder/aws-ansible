80662c87c317f1c9d8686ae3afe6bc8d9b8cb5ee
singh-ashok25 <ashok_iitr@yahoo.com>
2020-05-14 22:41:01 +0100

    upd pkg

--- packages.sh
+++ packages.sh
@@ -2,8 +2,12 @@
 yum install git -y
 sudo yum install python3 -y
 sudo amazon-linux-extras install ansible2 -y
+echo "==================================================="
 echo "RUN below command manually"
 echo python3 -m venv ansible
 echo source ~/aws-ansible/ansible/bin/activate
 echo pip install pip --upgrade
 echo pip install boto
+echo aws configure 
+echo ansible-playbook  -i inventory ec2.yml
+echo "==================================================="
