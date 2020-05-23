1268e91ef553471a7cde18a95981eef440d8afbe
singh-ashok25 <62803698+singh-ashok25@users.noreply.github.com>
2020-05-23 23:17:32 +0100

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -9,6 +9,7 @@ echo python3 -m venv ansible
 echo source ~/aws-ansible/ansible/bin/activate
 echo pip install pip --upgrade
 echo pip install boto
+echo pip install boto3
 echo aws configure 
 echo ansible-playbook  -i inventory ec2.yml
 echo "==================================================="
