947cb3614d8d4cb3bc1d642ecc66991034ba77d4
root <root@ip-172-31-6-79.ap-south-1.compute.internal>
2024-11-04 20:15:40 +0000

    changes to inventory and package

--- packages.sh
+++ packages.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-yum install git -y
-sudo yum install python3 -y
-sudo amazon-linux-extras install ansible2 -y
+# Install Git
+sudo dnf install git -y
+
+# Install Python 3
+sudo dnf install python3 -y
+
+# Install Ansible
+sudo dnf install ansible -y
+
 echo "==================================================="
-echo "RUN below command manually"
+echo "RUN below commands manually"
 echo "==================================================="
-echo python3 -m venv ansible
+echo python3 -m venv ~/aws-ansible/ansible
 echo source ~/aws-ansible/ansible/bin/activate
-echo pip install pip --upgrade
+echo pip install --upgrade pip
 echo pip install boto
 echo pip install boto3
 echo pip install ansible
 echo aws configure 
-echo ansible-playbook  -i inventory ec2.yml
+echo ansible-playbook -i inventory ec2.yml
 echo "==================================================="
+
